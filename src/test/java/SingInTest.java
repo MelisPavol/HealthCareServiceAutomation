6c8cb1138f0347b600bd4f46c20f7661f543da64
MelisPavol <117652829+MelisPavol@users.noreply.github.com>
2022-11-13 15:12:51 -0500

    add empty line and change variable TestValue

--- src/test/java/SingInTest.java
+++ src/test/java/SingInTest.java
@@ -14,6 +14,7 @@ public class SingInTest extends BaseTest{
 
             assertEquals("Make Appointment", appointmentPage.getTextMakeAppointment());
         }
+
     @Test
     void test_TC_01_Valid_SingIn_2(){
         var appointmentPage = homePage
@@ -24,6 +25,7 @@ public class SingInTest extends BaseTest{
 
         assertEquals("Make Appointment", appointmentPage.getTextMakeAppointment());
     }
+
     @Test
     void test_TC_2_Invalid_Sing_in(){
         var appointmentPage = homePage
@@ -34,6 +36,7 @@ public class SingInTest extends BaseTest{
 
         assertEquals("Login failed! Please ensure the username and password are valid.", appointmentPage.getTextLoginFailed());
     }
+
     @Test
     void test_TC_3_Valid_Sing_in_from_hamburger_menu(){
             var appointmentPage = homePage
@@ -45,6 +48,7 @@ public class SingInTest extends BaseTest{
 
         assertEquals("Make Appointment", appointmentPage.getTextMakeAppointment());
     }
+
     @Test
     void test_TC_4_Invalid_username(){
         var appointmentPage = homePage
@@ -55,6 +59,7 @@ public class SingInTest extends BaseTest{
 
         assertEquals("Login failed! Please ensure the username and password are valid.", appointmentPage.getTextLoginFailed());
     }
+
     @Test
     void test_TC_5_Invalid_password(){
         var appointmentPage = homePage
@@ -65,11 +70,12 @@ public class SingInTest extends BaseTest{
 
         assertEquals("Login failed! Please ensure the username and password are valid.", appointmentPage.getTextLoginFailed());
     }
+
     @Test
     void test_TC_6_Invalid_test_Empty_textfield_username() {
         var appointmentPage = homePage
                 .makeAppointmentButton()
-                .enterUserName(TestValues.TEST_INVALIDD_USERNAME_EMPTY)
+                .enterUserName(TestValues.TEST_INVALID_USERNAME_EMPTY)
                 .enterpassword(TestValues.TEST_VALID_PASSWORD)
                 .loginButtonOnLoginPage();
 
